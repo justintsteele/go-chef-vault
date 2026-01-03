851a355e0c19ce39dd706cf223b3ab8cd9f02d79
justinsteele <justin.steele@oracle.com>
2026-01-02 21:16:04 -0500

    feat: ✨ add support to DeleteItem for sparse keys

--- vault/delete.go
+++ vault/delete.go
@@ -2,6 +2,7 @@ package vault
 
 import (
 	"fmt"
+	"go-chef-vault/vault/item_keys"
 )
 
 type DeleteResponse struct {
@@ -30,20 +31,78 @@ func (s *Service) Delete(name string) (result *DeleteResponse, err error) {
 //   - Chef API Docs: https://docs.chef.io/api_chef_server/#delete-10
 //   - Chef-Vault Source: https://github.com/chef/chef-vault/blob/main/lib/chef/knife/vault_delete.rb
 func (s *Service) DeleteItem(name string, item string) (resp *DeleteResponse, err error) {
+	payload := &Payload{
+		VaultName:     name,
+		VaultItemName: item,
+	}
+	keyState, err := s.loadKeysCurrentState(payload)
+	if err != nil {
+		return
+	}
+
+	resp, err = s.deleteVaultItem(name, item)
+	if err != nil {
+		return
+	}
+
+	if keyState.Mode == item_keys.KeysModeDefault {
+		if err := s.deleteDefaultKeys(name, item, resp); err != nil {
+			return nil, err
+		}
+	} else if keyState.Mode == item_keys.KeysModeSparse {
+		if err := s.deleteSparseKeys(name, item, keyState, resp); err != nil {
+			return nil, err
+		}
+	}
+	return resp, nil
+}
+
+func (s *Service) deleteVaultItem(name string, item string) (resp *DeleteResponse, err error) {
 	itemUri := fmt.Sprintf("%s/%s", s.vaultURL(name), item)
+	if err := s.Client.DataBags.DeleteItem(name, item); err != nil {
+		return nil, err
+	}
 	resp = &DeleteResponse{
 		Response: Response{
 			URI: itemUri,
 		},
 	}
-	vErr := s.Client.DataBags.DeleteItem(name, item)
+	return
+}
 
-	// TODO: this needs to account for keys mode == sparse and delete the sparse keys as well
+func (s *Service) deleteDefaultKeys(name string, item string, out *DeleteResponse) (err error) {
 	itemKeysUri := fmt.Sprintf("%s/%s", s.vaultURL(name), item+"_keys")
-	kErr := s.Client.DataBags.DeleteItem(name, item+"_keys")
-	if vErr != nil || kErr != nil {
-		err = fmt.Errorf("vault error: %s, keys error: %s", vErr, kErr)
+	if err := s.Client.DataBags.DeleteItem(name, item+"_keys"); err != nil {
+		return err
 	}
-	resp.KeysURIs = append(resp.KeysURIs, itemKeysUri)
+	out.KeysURIs = append(out.KeysURIs, itemKeysUri)
+	return
+}
+
+func (s *Service) deleteSparseKeys(name string, item string, keyState *item_keys.VaultItemKeys, out *DeleteResponse) (err error) {
+	for _, admin := range keyState.Admins {
+		sparseId := fmt.Sprintf("%s_key_%s", item, admin)
+		adminKeyUri := fmt.Sprintf("%s/%s", s.vaultURL(name), sparseId)
+		if err := s.Client.DataBags.DeleteItem(name, sparseId); err != nil {
+			return err
+		}
+		out.KeysURIs = append(out.KeysURIs, adminKeyUri)
+	}
+
+	for _, client := range keyState.Clients {
+		sparseId := fmt.Sprintf("%s_key_%s", item, client)
+		clientKeyUri := fmt.Sprintf("%s/%s", s.vaultURL(name), sparseId)
+		if err := s.Client.DataBags.DeleteItem(name, sparseId); err != nil {
+			return err
+		}
+		out.KeysURIs = append(out.KeysURIs, clientKeyUri)
+	}
+
+	sparseId := fmt.Sprintf("%s_keys", item)
+	baseUri := fmt.Sprintf("%s/%s", s.vaultURL(name), sparseId)
+	if err := s.Client.DataBags.DeleteItem(name, sparseId); err != nil {
+		return err
+	}
+	out.KeysURIs = append(out.KeysURIs, baseUri)
 	return
 }

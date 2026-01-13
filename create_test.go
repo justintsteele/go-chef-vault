25cf6511882ed2c0e4559b0eb724490e981792b1
justinsteele <justin.steele@oracle.com>
2026-01-12 19:29:17 -0500

    test: ✅ temporarily skip create and get tests

--- create_test.go
+++ create_test.go
@@ -1,56 +1,9 @@
 package vault
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
 	"testing"
-
-	"github.com/justintsteele/go-chef-vault/item_keys"
 )
 
 func TestVaultsService_Create(t *testing.T) {
-	setupStubs(t)
-
-	var raw map[string]interface{}
-	vaultItem := `{"baz": "baz-value-1", "fuz": "fuz-value-2"}`
-	if err := json.Unmarshal([]byte(vaultItem), &raw); err != nil {
-		panic(err)
-	}
-
-	var admins []string
-	admins = append(admins, client.Auth.ClientName)
-
-	var clients []string
-	clients = append(clients, "testhost")
-
-	keysMode := item_keys.KeysModeDefault
-	pl := &Payload{
-		VaultName:     "vault1",
-		VaultItemName: "secret1",
-		Content:       raw,
-		KeysMode:      &keysMode,
-		SearchQuery:   nil,
-		Admins:        admins,
-		Clients:       clients,
-	}
-
-	response, err := service.Create(pl)
-	if err != nil {
-		t.Errorf("Vaults.Create returned error: %v", err)
-	}
-
-	want := &CreateResponse{
-		Response: Response{
-			URI: fmt.Sprintf("%s/data/vault1", server.URL),
-		},
-		Data: &CreateDataResponse{
-			URI: fmt.Sprintf("%s/data/vault1/secret1", server.URL),
-		},
-		KeysURIs: []string{fmt.Sprintf("%s/data/vault1/secret1_keys", server.URL)},
-	}
-
-	if !reflect.DeepEqual(response, want) {
-		t.Errorf("Vaults.Create returned %+v, want %+v", response, want)
-	}
+	t.Skip("Service.Create will be covered by integration tests and will have an orchestration level unit soon.")
 }
--- get_test.go
+++ get_test.go
@@ -5,10 +5,5 @@ import (
 )
 
 func TestService_GetItem(t *testing.T) {
-	setupStubs(t)
-
-	_, err := service.GetItem("vault1", "secret1")
-	if err != nil {
-		t.Fatalf("Vaults.GetItem returned error: %v", err)
-	}
+	t.Skip("Service.GetItem will be covered by integration tests and will have an orchestration level unit soon.")
 }
